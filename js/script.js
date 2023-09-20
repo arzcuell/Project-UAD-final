1ef9c75061660d9ad596a9f3281754b2c61bf5ce
Frenedy Arzuro <farzuro@gmail.com>
2023-09-19 20:17:59 -0400

    Primer commit: Se sube el proyecto al 90%.

---
+++ js/script.js
@@ -0,0 +1,83 @@
+// change navbar style on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>0);
+
+    // const logoStatic = document.querySelector('#waviy');
+    // if(window.scrollY > 0) {
+    //     logoStatic.classList.add('waviy-stop');
+    //     logoStatic.classList.remove('waviy');
+    // } else {
+    //     logoStatic.classList.remove('waviy-stop');
+    //     logoStatic.classList.add('waviy');
+    // }
+})
+
+//show or hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+
+        //change de icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus'
+        } else {
+            icon.className = 'uil uil-plus'
+        }
+    })
+})
+
+/* navmenu on screens less than 1024px*/
+
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style .display = "none";
+})
+
+//close nav menu
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+    // menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav);
+
+//dark-theme
+
+const icon = document.getElementById('toggle-menu-btn');
+
+
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    
+    if(document.body.classList.contains("dark-theme")) {
+        icon.innerHTML = "<i class='uil uil-sun'></i>";
+        localStorage.setItem('dark-theme', 'true');
+    }else {
+        icon.innerHTML = "<i class='uil uil-moon'></i>";
+        localStorage.setItem('dark-theme', 'false');
+    }    
+}
+
+// modo oscuro persistente
+
+if(localStorage.getItem('dark-theme') === 'true') {
+    document.body.classList.add("dark-theme");
+    icon.innerHTML = "<i class='uil uil-sun'></i>";
+} else {
+    document.body.classList.remove("dark-theme");
+    icon.innerHTML = "<i class='uil uil-moon'></i>";
+}
