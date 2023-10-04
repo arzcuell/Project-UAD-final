45ffefc707497e4aa5cbfbc35c5f5d55b13bea01
Frenedy Arzuro <farzuro@gmail.com>
2023-10-03 21:40:57 -0400

    Se agrega un popup con video mensaje de bienvenida

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ faqs.forEach(faq => {
 })
 
 
-/* navmenu on screens less than 1024px*/
+/* nav menu on screens less than 1024px*/
 const menu = document.querySelector('.nav__menu');
 const menuBtn = document.querySelector('#open-menu-btn');
 const closeBtn = document.querySelector('#close-menu-btn');
@@ -81,3 +81,54 @@ if(localStorage.getItem('dark-theme') === 'true') {
     document.body.classList.remove("dark-theme");
     icon.innerHTML = "<i class='uil uil-moon'></i>";
 }
+
+
+// Mostrar popup si la cookie no existe
+if (!getCookie("popupClosed")) {
+    document.querySelector(".popup").style.display = "block";
+  }
+  
+  // Escuchar clic en el botón de cerrar
+  document.querySelector(".close-btn").addEventListener("click", function() {
+    // Crear cookie con duración de 30 días
+    // setCookie("popupClosed", true, 30);
+    // Ocultar popup
+    document.querySelector(".popup").style.display = "none";
+    // Pausar video
+    document.querySelector(".popup-content video").pause();
+  });
+  
+  // Escuchar cambios en el checkbox
+  document.querySelector("#popup-checkbox").addEventListener("change", function() {
+    if (this.checked) {
+      // Crear cookie con duración de 60 días
+      setCookie("popupClosed", true, 60);
+      // Ocultar popup
+      document.querySelector(".popup").style.display = "none";
+      // Pausar video
+      document.querySelector(".popup-content video").pause();
+    }
+  });
+  
+  // Función para crear una cookie
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+  }
+  
+  // Función para obtener el valor de una cookie
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
